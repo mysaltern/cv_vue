707b3b48dbc6e46451153e30769015b679a6faa8
mysaltern <m.saltanatpouri@acmgroup.ir>
2020-05-13 15:52:35 +0430

    add component for header

---
+++ src/Pages/Shared/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <header id="site_header" class="header mobile-menu-hide">
+    <div class="header-content">
+      <div class="header-photo">
+        <img src="src/assets/img/mohammad.jpeg" alt="Alex Smith" />
+      </div>
+      <div class="header-titles">
+        <h2>Mohammad Saltanatpouri</h2>
+        <h4>Full stack Developer</h4>
+      </div>
+    </div>
+
+    <ul class="main-menu">
+      <li class="active">
+        <a href="#home" class="nav-anim">
+          <i class="fas fa-house-user"></i>
+          <span class="link-text">Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="#about-me" class="nav-anim">
+          <i class="far fa-smile"></i>
+          <span class="link-text">About Me</span>
+        </a>
+      </li>
+      <li>
+        <a href="#resume" class="nav-anim">
+          <span class="menu-icon lnr lnr-graduation-hat"></span>
+          <i class="far fa-address-card"></i>
+          <span class="link-text">Resume</span>
+        </a>
+      </li>
+      <li>
+        <a href="#portfolio" class="nav-anim">
+          <i class="fa fa-briefcase"></i>
+          <span class="link-text">Portfolio</span>
+        </a>
+      </li>
+      <li>
+        <a href="#blog" class="nav-anim">
+          <i class="fas fa-save"></i>
+          <span class="link-text">Sample</span>
+        </a>
+      </li>
+      <li>
+        <a href="#contact" class="nav-anim">
+          <i class="fas fa-mail-bulk"></i>
+          <span class="link-text">Contact</span>
+        </a>
+      </li>
+    </ul>
+
+    <div class="social-links">
+      <ul>
+        <li>
+          <a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+        <li>
+          <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li>
+          <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="header-buttons">
+      <a href="#" target="_blank" class="btn btn-primary">Download CV</a>
+    </div>
+
+    <div class="copyrights">© 2020 All rights reserved.</div>
+  </header>
+</template>
